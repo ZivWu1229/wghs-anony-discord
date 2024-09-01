a5c93866e1376b920f2eaa16637374f1a7ff8457
ZivWu <zivwu2852@gmail.com>
2024-09-01 09:50:53 +0800

    v0.1.1

--- auto_split.py
+++ auto_split.py
@@ -6,7 +6,7 @@ def split(text)->str:
     output=''
     char_count=0
     for v in text:
-        if ord(v)>=33 and ord(v)<=126:
+        if ord(v)>=32 and ord(v)<=126:
             char_count+=.5
         else:
             char_count+=1
@@ -18,7 +18,7 @@ def split(text)->str:
                 output+=v
         else:
             output+=v
-        if char_count >= 23:
+        if char_count >= 21:
             for char in range(len(output)-1,-1,-1):
                 if len(output)-char>=18 or output[char]=='\n':
                     output+='\n'
@@ -74,8 +74,9 @@ def auto_split(text)->list[str]:
 
 
 if __name__ == '__main__':
-    #text='Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world'
-    text='''更：我不知道上則編號（還沒發），總之我和朋友推坑某遊她不玩，直到她剛在學校認識新朋友喜歡玩，火速回坑（就是不是對遊戲沒興趣是對我沒興趣啦）而且他們剛認識時她跟我說那人說話像我（我要被取代惹）因為我很喜歡她，不想承認猜想。但我今天看到她貼文和那人今天出去玩，心直接涼掉，成為朋友將近一年她經常說：好想和妳出去，我也積極約她，但每次回應都是父母不允許，口氣也很誠懇和遺憾，因為她身邊朋友大多是一起長大的薇寶，也許她父母不認識我不放心。可今天無法再自欺欺人，真搞不懂；；她是我特別的朋友，我對她很真誠，結果後期一直忽冷忽熱：一邊用誠懇的語氣，一邊做這些事，明明說我是她重要的朋友，明明ig簡介@朋友列我也是前幾。還說訊息blabla，很難發現我發的（數天、數週才回我的人）但妳只是挑人回（親眼看到）我累了，不討厭妳甚至仍喜歡妳，但維持友誼的熱情被消滅殆盡'''
+    text='Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world Hello world'
+    #text='''更：我不知道上則編號（還沒發），總之我和朋友推坑某遊她不玩，直到她剛在學校認識新朋友喜歡玩，火速回坑（就是不是對遊戲沒興趣是對我沒興趣啦）而且他們剛認識時她跟我說那人說話像我（我要被取代惹）因為我很喜歡她，不想承認猜想。但我今天看到她貼文和那人今天出去玩，心直接涼掉，成為朋友將近一年她經常說：好想和妳出去，我也積極約她，但每次回應都是父母不允許，口氣也很誠懇和遺憾，因為她身邊朋友大多是一起長大的薇寶，也許她父母不認識我不放心。可今天無法再自欺欺人，真搞不懂；；她是我特別的朋友，我對她很真誠，結果後期一直忽冷忽熱：一邊用誠懇的語氣，一邊做這些事，明明說我是她重要的朋友，明明ig簡介@朋友列我也是前幾。還說訊息blabla，很難發現我發的（數天、數週才回我的人）但妳只是挑人回（親眼看到）我累了，不討厭妳甚至仍喜歡妳，但維持友誼的熱情被消滅殆盡'''
+    text='欸欸欸欸欸欸劉知珉為什麼可以那麼好看我愛死他救命然後他跟金旼炡可以不要那麼甜嗎煩死超生氣 哇啊啊啊啊啊我超愛他們謝謝 不客氣 超好看超好看超好看 很重要說三次 房時赫吃屎'
     output=split(text)
     print('----------------------------------------')
     print(output)
